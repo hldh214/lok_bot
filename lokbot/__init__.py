7a472418f22aba91151b06263260a7ea6b4dcb9b
hldh214 <hldh214@gmail.com>
2022-05-01 22:05:19 +0900

    feat(farmer): add rotation for logging

--- lokbot/__init__.py
+++ lokbot/__init__.py
@@ -1,4 +1,4 @@
-import logging
+import logging.handlers
 import pathlib
 import sys
 
@@ -11,11 +11,16 @@ builtin_logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-file_channel = logging.FileHandler(project_root.joinpath('builtin_logger.log'))
+file_channel = logging.handlers.TimedRotatingFileHandler(
+    project_root.joinpath('builtin_logger.log'),
+    interval=1,
+    when='D',
+    backupCount=1,
+)
 file_channel.setFormatter(formatter)
 
 builtin_logger.addHandler(file_channel)
 
 logger.remove()
-logger.add(project_root.joinpath('loguru.log'))
+logger.add(project_root.joinpath('loguru.log'), rotation='1 day', retention=2)
 logger.add(sys.stdout, colorize=True)
