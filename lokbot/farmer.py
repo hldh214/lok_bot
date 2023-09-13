777464758fe3a8d691bd164f4fcb9ebb2624b814
hldh214 <hldh214@gmail.com>
2023-09-13 13:32:01 +0900

    feat(farmer): sync `version`
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- lokbot/farmer.py
+++ lokbot/farmer.py
@@ -93,7 +93,7 @@ class LokFarmer:
             "country": "USA",
             "language": "English",
             "bundle": "",
-            "version": "1.1673.145.224",
+            "version": "1.1673.146.224",
             "platform": "web",
             "pushId": ""
         })
